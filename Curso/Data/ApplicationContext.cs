08e83b3e22b2e16745a79862a868ae02bde73fdc
Wladimir Ribeiro de Souza <wladimirsouza@outlook.com>
2021-12-29 08:10:58 -0300

    Aplicação concluída com tenttivas de conexão configurada.

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using Curso.Domain;
 using CursoEFCore.Data.Configurations;
 using Microsoft.EntityFrameworkCore;
@@ -14,16 +16,43 @@ namespace CursoEFCore.Data
 
        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
        {
-           
+           //A opção EnableRetryOnFailure informa quantas vezes a aplicação irá tentar se reconectar ao banco em caso de falha na conexão.
+           //Se não passar parâmetro algum, ele irá tentar até 6 vezes até completar 1 minuto.
+           //MaxRetryCount informa quantas vezes a aplicação poderá tentar se reconectar.
+           //MaxRetryDelay informa quanto tempo deve esperar até a aplicação tentar se reconectar novamente.
            optionsBuilder
            .UseLoggerFactory(_logger)
            .EnableSensitiveDataLogging()
-           .UseSqlServer("Server=localhost;Database=CursoEFCore;Trusted_Connection=True");
+           .UseSqlServer("Server=localhost;Database=CursoEFCore;Trusted_Connection=True",
+           p => p.EnableRetryOnFailure(maxRetryCount: 2, maxRetryDelay: TimeSpan.FromSeconds(5), errorNumbersToAdd: null)
+           .MigrationsHistoryTable("curso_ef_core"));
        } 
 
        protected override void OnModelCreating(ModelBuilder modelBuilder)
        {
            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+       }
+
+        //Esse método é para mapear propriedades que não foram configuradas.
+        //No exemplo abaixo iremos verificar as strings.
+       private void MapearPropriedadesEsquecidas(ModelBuilder modelBuilder)
+       {
+           foreach(var entity in modelBuilder.Model.GetEntityTypes())
+           {
+               var properties = entity.GetProperties().Where(p => p.ClrType == typeof(string));
+
+               foreach(var property in properties)
+               {
+                   if(string.IsNullOrEmpty(property.GetColumnType()) && !property.GetMaxLength().HasValue)
+                   {
+                       //Nesse caso irá verificar se uma propriedade do tipo string está vazia e irá setar o tamanho máximo
+                       property.SetMaxLength(100);
+
+                       //Aqui, irá configurar todas as propriedades do tipo string vazias como varchar(100)
+                       property.SetColumnType("VARCHAR(100)");
+                   }
+               }
+           }
        }
     }
 }
